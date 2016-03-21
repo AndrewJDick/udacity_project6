b8e1e148e676d94b510c38bfa97f60fbb05db094
Andrew.Dick <andrew.dick@cohaesus.co.uk>
2016-03-21 14:38:50 +0000

    feed name and url tests completed. Additional test suites added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,21 +27,40 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 
+        it('Feed URLs', function() {
+            allFeeds.forEach(function (feed) {
+                // Ensure URL is defined
+                expect(feed.url).toBeDefined();
 
-        /* TODO: Write a test that loops through each feed
+                // Ensure URLs are not empty
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
+
+
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it('Feed Names', function() {
+            allFeeds.forEach(function (feed) {
+                // Ensure URL is defined
+                expect(feed.name).toBeDefined();
+
+                // Ensure URLs are not empty
+                expect(feed.name.length).not.toBe(0);
+            });
+        });
     });
+    
 
-
-    /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -54,7 +73,7 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -63,10 +82,11 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
